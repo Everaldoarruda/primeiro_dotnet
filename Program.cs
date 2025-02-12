98c157eaada266cca9df71befe6f4a3cc909bab8
Everaldo arruda <dev.ejmarf@gmail.com>
2025-02-12 15:50:18 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("chegai Brasil!");
+
+string primeiroNome = "Aldo";
+Console.WriteLine(primeiroNome);
+
+string Aldo= "10anos";
+Console.Write(Aldo);
+
+
